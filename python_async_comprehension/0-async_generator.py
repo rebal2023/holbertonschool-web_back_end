ebacd047f29a2ca2fb5a19fec8b7aee966fc3f9d
Omar J <omarj819@gmail.com>
2024-04-22 20:01:47 +0300

    A

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     ''' Function that return a list '''
     for i in range(10):
         await asyncio.sleep(1)
