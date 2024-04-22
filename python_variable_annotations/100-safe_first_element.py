f38735815f5a6c3d90d3326c19bf46f890a19c1e
Omar J <omarj819@gmail.com>
2024-04-22 19:45:59 +0300

    A

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-'''Duck typing - first element of a sequence'''
-from typing import Union, Any, Sequence
 
+from typing import Sequence, Any, Optional
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    ''' Function safe_first_element '''
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Returns the first element of the sequence if it exists, otherwise None."""
     if lst:
         return (lst[0])
     else:
--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-'''More involved type annotations'''
-from typing import Sequence, Union, Any, TypeVar, Mapping
 
-R = TypeVar('T')
+from typing import Mapping, Any, TypeVar, Union
 
+# Define a type variable ~T
+T = TypeVar('~T')
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[R, None] = None) -> Union[Any, R]:
-    ''' Function safely_get_value '''
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Returns the value associated with the key in the dictionary if it exists, otherwise the default value."""
     if key in dct:
         return (dct[key])
     else:
         return (default)
-    
