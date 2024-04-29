8f9002b66435d3bf0e9fc21956d19584b9cb4b95
Omar J <omarj819@gmail.com>
2024-04-29 17:47:26 +0300

    A

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """log stats from collection
 """
-from pymongo import MongoClient
+from pymongo import MongoClient # type: ignore
 
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
